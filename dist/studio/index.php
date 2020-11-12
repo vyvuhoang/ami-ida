2e8825dc82fc2cf4f19e6b27de3890c19e02e7bb
Luu Hoang Bac <l.h.bac@alive-web.co.jp>
2020-11-12 22:43:19 +0700

    studio single

---
+++ dist/studio/index.php
@@ -0,0 +1,391 @@
+<?php
+$thisPageName = 'top';
+include_once(dirname(__DIR__) . '/app_config.php');
+include(APP_PATH.'libs/head.php');
+?>
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+<link rel="stylesheet" href="<?php echo APP_ASSETS ?>css/page/single-studio.min.css">
+</head>
+<body id="top" class="top">
+<?php include(APP_PATH.'libs/header.php'); ?>
+<div id="wrap">
+  <main>
+    <div class="sec-intro">
+      <div class="container-1080">
+        <div class="inside">
+          <h3 class="topic-ttl">９月限定！〇〇店の入会特典</h3>
+          <ul class="lst-intro">
+            <li class="item">
+              <div class="txt">
+                <p class="txt01">入会特典<br>月額会費</p>
+                <p class="txt02">半額</p>
+              </div>
+            </li>
+            <li class="item">
+              <div class="txt">
+                <p class="txt01">入会特典<br>入会金</p>
+                <p class="price">
+                  <span class="number">0</span>
+                  <span class="unit">円</span>
+                </p>
+              </div>
+            </li>
+            <li class="item">
+              <div class="txt">
+                <p class="txt01">入会特典<br>水素飲み放題</p>
+                <p class="price">
+                  <span class="number">0</span>
+                  <span class="unit">円</span>
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="sec-btns">
+      <div class="container-1080">
+        <a href="" class="btn"><span>WEB入会金受付はこちら</span></a>
+        <a href="" class="btn"><span>体験レッスンへのご参加はこちら</span></a>
+      </div>
+    </div>
+    <div class="sec-charge">
+      <div class="container-900">
+        <h3 class="topic-ttl">プラン・料金のご説明</h3>
+        <ul class="lst-price js-lst-price">
+          <li class="item">
+            <h4 class="item-ttl js-price-ttl">
+              <span class="txt01">マンスリーオプション</span>
+              <span class="txt02">※一部店舗</span>
+            </h4>
+            <div class="tbl js-price-tbl">
+              <div class="row">
+                <p class="th">水素水</p>
+                <p class="td01">1,000円</p>
+                <p class="td02">水素水飲み放題。専用ボトルにてご利用可能</p>
+              </div>
+              <div class="row">
+                <p class="th">タオル</p>
+                <p class="td01">1,000円</p>
+                <p class="td02">1日1回、バスタオル1枚とフェイスタオル1枚がご利用可能</p>
+              </div>
+              <div class="row">
+                <p class="th">水素水</p>
+                <p class="td01">1,000円</p>
+                <p class="td02">水素水飲み放題。専用ボトルにてご利用可能</p>
+              </div>
+            </div>
+          </li>
+          <li class="item">
+            <h4 class="item-ttl js-price-ttl">
+              <span class="txt01">オプション</span>
+            </h4>
+            <div class="tbl js-price-tbl">
+              <div class="row">
+                <p class="th">水素水</p>
+                <p class="td01">1,000円</p>
+                <p class="td02">水素水飲み放題。専用ボトルにてご利用可能</p>
+              </div>
+              <div class="row">
+                <p class="th">タオル</p>
+                <p class="td01">1,000円</p>
+                <p class="td02">1日1回、バスタオル1枚とフェイスタオル1枚がご利用可能</p>
+              </div>
+              <div class="row">
+                <p class="th">水素水</p>
+                <p class="td01">1,000円</p>
+                <p class="td02">水素水飲み放題。専用ボトルにてご利用可能</p>
+              </div>
+            </div>
+          </li>
+          <li class="item">
+            <h4 class="item-ttl js-price-ttl">
+              <span class="txt01">オプション（アミ―ダオリジナルウェア）</span>
+              <span class="txt02">※一部店舗</span>
+            </h4>
+            <div class="tbl js-price-tbl">
+              <div class="row">
+                <p class="th">水素水</p>
+                <p class="td01">1,000円</p>
+                <p class="td02">水素水飲み放題。専用ボトルにてご利用可能</p>
+              </div>
+              <div class="row">
+                <p class="th">タオル</p>
+                <p class="td01">1,000円</p>
+                <p class="td02">1日1回、バスタオル1枚とフェイスタオル1枚がご利用可能</p>
+              </div>
+              <div class="row">
+                <p class="th">水素水</p>
+                <p class="td01">1,000円</p>
+                <p class="td02">水素水飲み放題。専用ボトルにてご利用可能</p>
+              </div>
+            </div>
+          </li>
+          <li class="item">
+            <h4 class="item-ttl js-price-ttl">
+              <span class="txt01">レンタル</span>
+              <span class="txt02">※一部店舗</span>
+            </h4>
+            <div class="tbl js-price-tbl">
+              <div class="row">
+                <p class="th">水素水</p>
+                <p class="td01">1,000円</p>
+                <p class="td02">水素水飲み放題。専用ボトルにてご利用可能</p>
+              </div>
+              <div class="row">
+                <p class="th">タオル</p>
+                <p class="td01">1,000円</p>
+                <p class="td02">1日1回、バスタオル1枚とフェイスタオル1枚がご利用可能</p>
+              </div>
+              <div class="row">
+                <p class="th">水素水</p>
+                <p class="td01">1,000円</p>
+                <p class="td02">水素水飲み放題。専用ボトルにてご利用可能</p>
+              </div>
+            </div>
+          </li>
+        </ul>
+        <p class="script">※体験料以外の表示価格は全て税抜きとなります。水素水のみ消費税8％</p>
+      </div>
+    </div>
+    <div class="sec-stuff">
+      <div class="container-1080">
+        <h3 class="topic-ttl">体験レッスンに必要な物追加</h3>
+        <ul class="lst-stuff">
+          <li class="item">
+            <div class="img"><img src="<?php echo APP_ASSETS;?>img/studio/img02.jpg" alt="手ぶらでOK"></div>
+            <h4 class="item-ttl">手ぶらでOK</h4>
+            <p class="item-txt">必要なものは全てご用意しています。 身軽だから気軽に通いやすいのが特徴です。<br>※キャミソール(カップ付き)かTシャツのいずれかになります。 ※店舗により異なります。<br>ロレンタルウェア上下 セバスタオル<br>フェイスタオル 四水550ml×2本 四ヨガマット</p>
+          </li>
+          <li class="item">
+            <div class="img"><img src="<?php echo APP_ASSETS;?>img/studio/img03.jpg" alt="好きな時間に 通えます"></div>
+            <h4 class="item-ttl">好きな時間に 通えます</h4>
+            <p class="item-txt">昼間に自由時間がある方にも、 仕事で遅くなりがちな方にも、 通いやすいのが魅力です。<br>※レッスン時間はスタジオ・曜日によって異なります。<br>詳しくはスタジオ詳細でご確認ください。</p>
+          </li>
+          <li class="item">
+            <div class="img"><img src="<?php echo APP_ASSETS;?>img/studio/img04.jpg" alt="女性専用 スタジオ"></div>
+            <h4 class="item-ttl">女性専用 スタジオ</h4>
+            <p class="item-txt">女性だけのスタジオだから、 気兼ねなく、たくさん汗かいたり ポーズを取ったりできるのも、 支持されている理由のひとつです。</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="sec-feeling">
+      <div class="container-1080">
+        <h3 class="topic-ttl">アミーダの溶岩ホットヨガで、<br>感じられている効果</h3>
+        <ul class="lst-feeling">
+          <li class="item">
+            <div class="img"><img src="<?php echo APP_ASSETS;?>img/studio/img05.png" alt=""></div>
+            <div class="name">体質・体型が変わった<br>の円グラフ</div>
+          </li>
+          <li class="item">
+            <div class="img"><img src="<?php echo APP_ASSETS;?>img/studio/img05.png" alt=""></div>
+            <div class="name">体質・体型が変わった<br>の円グラフ</div>
+          </li>
+          <li class="item">
+            <div class="img"><img src="<?php echo APP_ASSETS;?>img/studio/img05.png" alt=""></div>
+            <div class="name">体質・体型が変わった<br>の円グラフ</div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="sec-voice">
+      <h3 class="topic-ttl">アミーダの溶岩ホットヨガを選ぶお客様のお声</h3>
+      <div class="slider js-voice-slider">
+        <div class="item" style="background-image: url(<?php echo APP_ASSETS;?>img/studio/img06.jpg);"></div>
+        <div class="item" style="background-image: url(<?php echo APP_ASSETS;?>img/studio/img06.jpg);"></div>
+        <div class="item" style="background-image: url(<?php echo APP_ASSETS;?>img/studio/img06.jpg);"></div>
+        <div class="item" style="background-image: url(<?php echo APP_ASSETS;?>img/studio/img06.jpg);"></div>
+        <div class="item" style="background-image: url(<?php echo APP_ASSETS;?>img/studio/img06.jpg);"></div>
+      </div>
+      <div class="container-1080">
+        <ul class="lst-voice">
+          <li class="item">
+            <div class="avatar"><img src="<?php echo APP_ASSETS;?>img/studio/img07.jpg" alt=""></div>
+            <div class="voice">
+              <div class="bg">
+                <h4 class="voice-ttl">アミーダは、なくてはならない生活の一部。</h4>
+                <p class="voice-txt">ホットヨガ初心者だったので、ついていけないかも…とかなり不安でしたが、行ってみたら他にも初心者の方が何人もいて、インストラクターの方が丁寧にフォローしてくれたので無駄な心配でしたね。初めての身体の温まり方に最初はヘトヘトでしたが、今は汗をかいてるのが快感です！</p>
+                <p class="info">20代女性｜会社員</p>
+              </div>
+            </div>
+          </li>
+          <li class="item">
+            <div class="avatar"><img src="<?php echo APP_ASSETS;?>img/studio/img07.jpg" alt=""></div>
+            <div class="voice">
+              <div class="bg">
+                <h4 class="voice-ttl">アミーダは、なくてはならない生活の一部。</h4>
+                <p class="voice-txt">ホットヨガ初心者だったので、ついていけないかも…とかなり不安でしたが、行ってみたら他にも初心者の方が何人もいて、インストラクターの方が丁寧にフォローしてくれたので無駄な心配でしたね。初めての身体の温まり方に最初はヘトヘトでしたが、今は汗をかいてるのが快感です！</p>
+                <p class="info">20代女性｜会社員</p>
+              </div>
+            </div>
+          </li>
+          <li class="item">
+            <div class="avatar"><img src="<?php echo APP_ASSETS;?>img/studio/img07.jpg" alt=""></div>
+            <div class="voice">
+              <div class="bg">
+                <h4 class="voice-ttl">アミーダは、なくてはならない生活の一部。</h4>
+                <p class="voice-txt">ホットヨガ初心者だったので、ついていけないかも…とかなり不安でしたが、行ってみたら他にも初心者の方が何人もいて、インストラクターの方が丁寧にフォローしてくれたので無駄な心配でしたね。初めての身体の温まり方に最初はヘトヘトでしたが、今は汗をかいてるのが快感です！</p>
+                <p class="info">20代女性｜会社員</p>
+              </div>
+            </div>
+          </li>
+        </ul>
+        <div class="btns">
+          <a href="" class="btn">WEB入会金受付はこちら</a>
+          <a href="" class="btn">体験レッスンへのご参加はこちら</a>
+        </div>
+      </div>
+    </div>
+    <div class="sec-reason">
+      <div class="container-1080">
+        <h3 class="topic-ttl">アミーダ〇〇店が選ばれる理由</h3>
+        <ul class="lst-reason">
+          <li class="item">
+            <div class="img"><img src="<?php echo APP_ASSETS;?>img/studio/img09.jpg" alt=""></div>
+            <h4 class="item-ttl">会員様8割がホットヨガ初体験</h4>
+            <p class="item-txt">ほとんどの方が0からのスタート。<br>カラダが硬くても大丈夫！！初心者の方が安心して受けられるプログラムが多数。</p>
+          </li>
+          <li class="item">
+            <div class="img"><img src="<?php echo APP_ASSETS;?>img/studio/img09.jpg" alt=""></div>
+            <h4 class="item-ttl">会員様8割がホットヨガ初体験</h4>
+            <p class="item-txt">ほとんどの方が0からのスタート。<br>カラダが硬くても大丈夫！</p>
+          </li>
+          <li class="item">
+            <div class="img"><img src="<?php echo APP_ASSETS;?>img/studio/img09.jpg" alt=""></div>
+            <h4 class="item-ttl">会員様8割がホットヨガ初体験</h4>
+            <p class="item-txt">ほとんどの方が0からのスタート。<br>カラダが硬くても大丈夫！</p>
+          </li>
+          <li class="item">
+            <div class="img"><img src="<?php echo APP_ASSETS;?>img/studio/img09.jpg" alt=""></div>
+            <h4 class="item-ttl">会員様8割がホットヨガ初体験</h4>
+            <p class="item-txt">ほとんどの方が0からのスタート。<br>カラダが硬くても大丈夫！</p>
+          </li>
+          <li class="item">
+            <div class="img"><img src="<?php echo APP_ASSETS;?>img/studio/img09.jpg" alt=""></div>
+            <h4 class="item-ttl">会員様8割がホットヨガ初体験</h4>
+            <p class="item-txt">ほとんどの方が0からのスタート。<br>カラダが硬くても大丈夫！</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="sec-lesson">
+      <div class="bg"></div>
+      <div class="container-1170">
+        <div class="outside">
+          <h3 class="topic-ttl">アミーダ〇〇店 レッスンスケジュール</h3>
+          <div class="inside">
+            <ul class="lst-lesson">
+              <li class="item">
+                <div class="img"><img src="<?php echo APP_ASSETS;?>img/studio/img11.jpg" alt=""></div>
+                <div class="content">
+                  <h4 class="content-ttl">テキストテキストテキストテキストテキストテキテキストテキストテキストテキストテ</h4>
+                  <p class="content-txt">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストキストテキストキストテキストキストテキストキストテキスト</p>
+                </div>
+              </li>
+              <li class="item">
+                <div class="img"><img src="<?php echo APP_ASSETS;?>img/studio/img11.jpg" alt=""></div>
+                <div class="content">
+                  <h4 class="content-ttl">テキストテキストテキストテキストテキストテキテキストテキストテキストテキストテ</h4>
+                  <p class="content-txt">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストキストテキストキストテキストキストテキストキストテキスト</p>
+                </div>
+              </li>
+              <li class="item">
+                <div class="img"><img src="<?php echo APP_ASSETS;?>img/studio/img11.jpg" alt=""></div>
+                <div class="content">
+                  <h4 class="content-ttl">テキストテキストテキストテキストテキストテキテキストテキストテキストテキストテ</h4>
+                  <p class="content-txt">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストキストテキストキストテキストキストテキストキストテキスト</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="sec-access">
+      <div class="container-1080">
+        <h3 class="topic-ttl">アミーダ〇〇店へのアクセス</h3>
+        <div class="access">
+          <div class="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.604743966393!2d139.74546941539734!3d35.7359381345767!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188d77608f7d37%3A0xe6baada2046b743e!2z5rq25bKp44Ob44OD44OI44Oo44Ks44K544K_44K444KqIOOCouODn-ODvOODgOmnkui-vOW6lw!5e0!3m2!1sen!2s!4v1605192912814!5m2!1sen!2s" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+          </div>
+          <div class="info">
+            <div class="inside">
+              <div class="row">
+                <div class="th">所在地</div>
+                <div class="td">〒170-0003 <br>東京都豊島区駒込1-38-2 <br>駒込TRビル4階</div>
+              </div>
+              <div class="row">
+                <div class="th">TEL</div>
+                <div class="td">03-5981-9027</div>
+              </div>
+              <div class="row">
+                <div class="th">FAX</div>
+                <div class="td">03-5981-9028</div>
+              </div>
+              <div class="row">
+                <div class="th"></div>
+                <div class="td">最寄駅		1<br>最寄駅		2<br>最寄駅		3</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="sec-faq">
+      <div class="container-900">
+        <h3 class="topic-ttl">溶岩ホットヨガ / アミーダに関するよくある質問</h3>
+        <ul class="lst-faq js-lst-faq">
+          <li class="item">
+            <div class="ques js-ques"><span>溶岩石ホットヨガの「痩せる効果」が高い理由は？</span></div>
+            <div class="ans js-ans">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</div>
+          </li>
+          <li class="item">
+            <div class="ques js-ques"><span>溶岩石ホットヨガの「痩せる効果」が高い理由は？</span></div>
+            <div class="ans js-ans">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</div>
+          </li>
+          <li class="item">
+            <div class="ques js-ques"><span>溶岩石ホットヨガの「痩せる効果」が高い理由は？</span></div>
+            <div class="ans js-ans">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</div>
+          </li>
+          <li class="item">
+            <div class="ques js-ques"><span>溶岩石ホットヨガの「痩せる効果」が高い理由は？</span></div>
+            <div class="ans js-ans">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</div>
+          </li>
+          <li class="item">
+            <div class="ques js-ques"><span>溶岩石ホットヨガの「痩せる効果」が高い理由は？</span></div>
+            <div class="ans js-ans">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</div>
+          </li>
+          <li class="item">
+            <div class="ques js-ques"><span>溶岩石ホットヨガの「痩せる効果」が高い理由は？</span></div>
+            <div class="ans js-ans">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</div>
+          </li>
+          <li class="item">
+            <div class="ques js-ques"><span>溶岩石ホットヨガの「痩せる効果」が高い理由は？</span></div>
+            <div class="ans js-ans">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="sec-ins">
+      <div class="container-1080">
+        <h3 class="topic-ttl">インスタグラム反映</h3>
+        <ul class="lst-ins">
+          <li class="item"><img src="<?php echo APP_ASSETS;?>img/studio/img10.jpg" alt=""></li>
+          <li class="item"><img src="<?php echo APP_ASSETS;?>img/studio/img10.jpg" alt=""></li>
+          <li class="item"><img src="<?php echo APP_ASSETS;?>img/studio/img10.jpg" alt=""></li>
+          <li class="item"><img src="<?php echo APP_ASSETS;?>img/studio/img10.jpg" alt=""></li>
+          <li class="item"><img src="<?php echo APP_ASSETS;?>img/studio/img10.jpg" alt=""></li>
+          <li class="item"><img src="<?php echo APP_ASSETS;?>img/studio/img10.jpg" alt=""></li>
+          <li class="item"><img src="<?php echo APP_ASSETS;?>img/studio/img10.jpg" alt=""></li>
+          <li class="item"><img src="<?php echo APP_ASSETS;?>img/studio/img10.jpg" alt=""></li>
+        </ul>
+      </div>
+    </div>
+  </main>
+</div>
+<?php include(APP_PATH.'libs/footer.php'); ?>
+<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script src="<?php echo APP_ASSETS ?>js/page/single-studio.min.js"></script>
+</body>
+</html>
