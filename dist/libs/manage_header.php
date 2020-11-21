48e9ba5e8db7c758891c99d64a4b366e7c5636bd
Luu Hoang Bac <l.h.bac@alive-web.co.jp>
2020-11-21 20:05:15 +0700

    manage index

---
+++ dist/libs/manage_header.php
@@ -0,0 +1,8 @@
+<header class="mheader">
+  <div class="container-1140">
+    <div class="ttl"><?php echo $page_ttl?></div>
+    <div class="right">
+      <div class="btn-setting">アカウント設定</div>
+    </div>
+  </div>
+</header>
