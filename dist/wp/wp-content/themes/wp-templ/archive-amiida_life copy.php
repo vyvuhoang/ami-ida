5b24fcf5fdd9f54a29d454167e592f9c689711ab
minhspom <l.n.minh@a-livesystems.com>
2020-11-24 08:52:47 +0700

    amiidalife

--- dist/wp/wp-content/themes/wp-templ/archive-amiida_life copy.php
+++ dist/wp/wp-content/themes/wp-templ/archive-amiida_life copy.php
@@ -24,26 +24,23 @@ include(APP_PATH.'libs/head.php');
       <?php for($i=0;$i<5;$i++){ ?>
       <div class="item">
         <div class="item__img">
-          <div class="item--ttl sp">
-            <a href="" class="ttl1">めまぐるしく<em>過ぎていく<br class="sp">日々</em>のなか。</a>
-          </div>
+          <a href="" class="item__info--ttl sp">
+            <p class="ttl1">ここに、アミーダの<br>あるライフスタイルを</p>
+            <p class="ttl2">引き立てるようなキャッチが入ります。</p>
+          </a>
           <div class="img lazy" data-bg="url(<?php echo APP_ASSETS; ?>img/amiidalife/img1.jpg)"></div>
         </div>
         <div class="item__info">
-          <div class="item--ttl pc">
-            <a href="" class="ttl1">めまぐるしく<em>過ぎていく<br class="sp">日々</em>のなか。</a>
-          </div>
+          <div class="item__info--space"></div>
+          <a href="" class="item__info--ttl pc">
+            <p class="ttl1">ここに、アミーダの<br>あるライフスタイルを</p>
+            <p class="ttl2">引き立てるようなキャッチが入ります。</p>
+          </a>
           <div class="item__info--space"></div>
           <div class="item__info--img">
             <div class="img lazy" data-bg="url(<?php echo APP_ASSETS; ?>img/amiidalife/img2.jpg)"></div>
           </div>
         </div>
-       <!--  <div class="line">
-          <div class="space"> </div>
-          <div class="inner">
-            <div class="circle1"></div><div class="circle2"></div>
-          </div>
-        </div> -->
       </div>
     <?php } ?>
     </div>
